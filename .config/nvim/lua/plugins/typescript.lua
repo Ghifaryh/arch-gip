0d3e9d18be596b86884995f78ebdae65a99ae8dc
Ghifary <mhghifaryy@gmail.com>
2025-09-24 10:19:44 +0700

    Add TypeScript tools plugin configuration

---
+++ .config/nvim/lua/plugins/typescript.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "pmizio/typescript-tools.nvim",
+    ft = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+  },
+}
